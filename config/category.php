c0d87e3e16590c26ba49f3c30e5982a42d0c2168
Niels Hamelink <niels@gyvex.com>
2025-07-22 16:26:37 +0200

    Added config options

--- config/category.php
+++ config/category.php
@@ -1,5 +1,16 @@
 <?php
 
-return [
+use VeiligLanceren\LaravelMorphCategories\Models\Category;
+use VeiligLanceren\LaravelMorphCategories\Models\Categoryable;
 
+return [
+    'tables' => [
+        'categories' => 'categories',
+        'categoryables' => 'categoryables',
+    ],
+
+    'models' => [
+        'category' => Category::class,
+        'categoryable' => Categoryable::class,
+    ],
 ];
